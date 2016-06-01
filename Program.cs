7960a1ab83ee2268d3ba5fb506288166119236c5
Michal Kaluzinski <rammis94@gmail.com>
2016-06-01 13:16:28 +0200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+namespace ListaList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<List<int>> lista = new List<List<int>>();
+            Random random = new Random();
+            
+
+            for (int i = 0; i < 5; i++)
+            {
+            lista.Add(new List<int>());
+
+
+                for (int j = 0; j < 3; j++)
+                {
+                    lista[i].Add(random.Next());
+                    
+                }
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("[ " + i + " ]");
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(" " + lista[i][j]);
+                }
+                Console.Write("\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
